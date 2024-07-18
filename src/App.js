26f9320d80a701af6f0e9616d5e0fd9182c500b5
Osman <Naasir.Osman@mhra.gov.uk>
2024-07-18 16:56:52 +0100

    changed src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Welcome to MHRA</code>
+          <code>NOT Welcome to MHRA</code>
         </p>
         <a
           className="App-link"
